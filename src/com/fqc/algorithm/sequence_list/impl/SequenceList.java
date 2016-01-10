4472bf5cde72af075c9063d7bb4e0df20d6631e1
fqc <337940626@qq.com>
2016-01-10 21:40:40 +0800

    重构构造方法,重载构造

--- src/com/fqc/algorithm/sequence_list/impl/SequenceList.java
+++ src/com/fqc/algorithm/sequence_list/impl/SequenceList.java
@@ -5,7 +5,26 @@ import com.fqc.algorithm.sequence_list.List;
 /**
  * Created by fqc on 2016/1/9.
  */
-public class Sequence_List implements List{
+public class SequenceList implements List{
+
+
+    static final int defaultSize = 10;
+    int size;
+    int maxSize;
+    Object[] objects;
+
+    public SequenceList() {
+        this(defaultSize);
+    }
+
+    public SequenceList(int size) {
+        this.init(size);
+    }
+
+    public void init(int size) {
+        this.size = size;
+    }
+
     @Override
     public int size() {
         return 0;
