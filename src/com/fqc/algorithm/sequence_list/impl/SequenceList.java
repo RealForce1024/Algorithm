c1ef3cdfb2b0e330260305ed1e882afa935b1db0
fqc <337940626@qq.com>
2016-03-02 23:22:03 +0800

    增加线性表顺序结构方式 空实现，定义了一些该有的属性

--- src/com/fqc/algorithm/sequence_list/impl/SequenceList.java
+++ src/com/fqc/algorithm/sequence_list/impl/SequenceList.java
@@ -4,37 +4,25 @@ import com.fqc.algorithm.sequence_list.List;
 
 /**
  * Created by fqc on 2016/1/9.
+ * 顺序结构实现线性表
  */
 public class SequenceList implements List{
 
-
-    static final int defaultSize = 10;
-    int size;
+    final int defaultSize = 10;
     int maxSize;
-    Object[] objects;
+    //当前长度
+    int size;
+    //对象数组(保存当前对象，)
+    Object[] listArray;
 
-    public SequenceList() {
-        this(defaultSize);
-    }
-
-    public SequenceList(int size) {
-        this.init(size);
-    }
-
-    public void init(int size) {
-        this.size = size;
-    }
 
     @Override
     public int size() {
-        return size;
+        return 0;
     }
 
     @Override
     public boolean isEmpty() {
-        if (this.size == 0) {
-            return true;
-        }
         return false;
     }
 
