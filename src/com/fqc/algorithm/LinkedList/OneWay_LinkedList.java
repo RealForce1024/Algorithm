35428a4310431f033d7e9470a04262a7cba86c25
fqc <337940626@qq.com>
2016-03-13 23:06:05 +0800

    add some ignore item after imported eclipse.

--- src/com/fqc/algorithm/LinkedList/OneWay_LinkedList.java
+++ src/com/fqc/algorithm/LinkedList/OneWay_LinkedList.java
@@ -4,19 +4,17 @@ package com.fqc.algorithm.LinkedList;
  * Created by fqc on 2016/1/5.
  */
 public class OneWay_LinkedList {
-    
+
 }
 
+class ListNode {
 
-class ListNode{
+	ListNode next;
+	int value;
 
-    ListNode next;
-    int value;
+	public ListNode(int value) {
+		this.value = value;
+		this.next = null;
+	}
 
-    public ListNode(int value) {
-        this.value = value;
-        this.next = null;
-    }
-
-    
 }
