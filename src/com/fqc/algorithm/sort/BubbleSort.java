3c7572c83a4e95b6ac7f8c7c5fda33accbaaffe9
fqc <337940626@qq.com>
2016-06-09 15:09:18 +0800

    udpate commont for bubble sort

--- src/com/fqc/algorithm/sort/BubbleSort.java
+++ src/com/fqc/algorithm/sort/BubbleSort.java
@@ -9,8 +9,8 @@ public class BubbleSort {
         int[] arrs = {1, 3, 5, 2, 4, 6};
         int temp = 0; //temp作为临时变量,方便大数向后交换
 
-        for (int i = 0; i < arrs.length - 1; i++) {   //arrs.length-1 要排序的次数
-            for (int j = 0; j < arrs.length - 1 - i; j++) { //未完成排序的次数
+        for (int i = 0; i < arrs.length - 1; i++) {   //arrs.length-1 ->要排序的次数
+            for (int j = 0; j < arrs.length - 1 - i; j++) { // arrs.length - 1 - i ->未完成排序的次数
                 if (arrs[j] > arrs[j + 1]) {
                     temp = arrs[j];
                     arrs[j] = arrs[j + 1];
