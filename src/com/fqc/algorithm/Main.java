19057dc83672b83ba6066dbfa722960f77d8805b
fqc <337940626@qq.com>
2016-08-04 21:21:31 +0800

    优酷面试官问的垃圾问题 try catch finally 中 try return 是否还会执行finally

--- src/com/fqc/algorithm/Main.java
+++ src/com/fqc/algorithm/Main.java
@@ -3,8 +3,22 @@ package com.fqc.algorithm;
 public class Main {
 
     public static void main(String[] args) {
-	//write your code here
-
+        //write your code here
+        int i = test();
+        System.out.println(i);//0
     }
-    
+    //一优酷面试官问的垃圾问题,实际中有人这样写?
+    private static int test() {
+        int i = 0;
+        try {
+            return i;
+        } catch (Exception ex) {
+
+        } finally {
+            i++;
+        }
+        return i;
+    }
+
+
 }
